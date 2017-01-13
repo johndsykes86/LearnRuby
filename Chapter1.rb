b8a3e212b0e279aaa54ebcc41d57ff2bcef33b95
Johnathon Sykes <johndsykes86@gmail.com>
2017-01-12 19:42:38 -0800

    initial commit: first and second problem of Chapter 1

---
+++ Chapter1.rb
@@ -0,0 +1,15 @@
+# Write a program which tells you:
+#
+# how many hours are in a year?
+# how many minutes are in a decade?
+# how many seconds old are you?
+# how many chocolates do you hope to eat in your life?
+
+
+#How many hours in a day.
+
+hours_in_year = 365 * 24
+puts "There are #{hours_in_year} hours in a year."
+
+minutes_in_decade = 365 * 10 * 24 * 60
+puts "There are #{minutes_in_decade} minutes in a decade."
