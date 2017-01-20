b43fc7034e2f94a3d372fb5d452b011a57ba42e3
Johnathon Sykes <johndsykes86@gmail.com>
2017-01-20 09:45:34 -0800

    Added Table of Contents solution to Chapter 5

---
+++ Chapter 5/toc.rb
@@ -0,0 +1,5 @@
+lineWidth = 75
+puts "Table of Contents".center(lineWidth)
+puts 'Chapter 1: '.ljust(lineWidth/5) + "Numbers".ljust(lineWidth/3) + "page 1".rjust(lineWidth/3)
+puts 'Chapter 2: '.ljust(lineWidth/5) + "Letters".ljust(lineWidth/3) + "page 78".rjust(lineWidth/3)
+puts 'Chapter 3: '.ljust(lineWidth/5) + "Variables".ljust(lineWidth/3) + "page 118".rjust(lineWidth/3)
