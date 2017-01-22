aa6d032d57f680339233a7af4b6b9612ee1bea72
Johnathon Sykes <johndsykes86@gmail.com>
2017-01-22 12:17:41 -0800

    corrected solution to reflect best practices

--- Chapter 6/deafgrandma.rb
+++ Chapter 6/deafgrandma.rb
@@ -1,17 +1,12 @@
 
+say = "hi grammy"
 
-  bye_count = 0
-
-
-  puts 'Helloooo! Give your old grandma a hug!!! How have you been!?!'
-  response = gets.chomp
-  while (response != "BYE")
-    puts "HUH?! SPEAK UP, SONNY!"
-    response = gets.chomp
-
-  if (response == response.upcase)
-    year = rand(1930..1950)
-    puts 'NO! NOT SINCE ' + year.to_s + "!"
-    response = gets.chomp
-   end
+while say != "BYE"
+  say = gets.chomp
+  while say != say.upcase
+    puts 'HUH? SPEAK UP SONNY!'
+    say = gets.chomp
   end
+    year = rand(1930..1950)
+    puts 'NO, NOT SINCE ' + year.to_s
+end
